81d90eaf29d07da2a1b3decb6218292cca944e39
SabbirHossainEvan <evansabbir31@gmail.com>
2025-10-09 21:01:54 +0600

    my 2nd commit

---
+++ src/components/LatestNews.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LatestNews = () => {
+    return (
+        <div className='flex items-center gap-5 bg-base-200 p-3'>
+            <p className='text-base-100 bg-secondary px-3 py-2'>Latest</p>
+            <p className='font-bold'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, facilis.</p>
+        </div>
+    );
+};
+
+export default LatestNews;
